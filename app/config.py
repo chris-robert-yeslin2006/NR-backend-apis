cf3f46dc6edaf7565f567ccc3b9da0997367132c
chris-robert-yeslin2006 <robertchemist2006@gmail.com>
2025-09-04 18:04:05 +0530

    redis cloud hosted

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ from pydantic import Field
 from typing import List
 
 class Settings(BaseSettings):
-    redis_url: str = Field("redis://localhost:6379", env="REDIS_URL")
+    redis_url: str = Field("redis://chris:Robert@2006@redis-18998.c80.us-east-1-2.ec2.redns.redis-cloud.com:18998", env="REDIS_URL")
     file_upload_max_size: int = Field(100_000_000, env="FILE_UPLOAD_MAX_SIZE")
     allowed_file_extensions: str = Field("csv,json,xlsx,txt", env="ALLOWED_FILE_EXTENSIONS")
     temp_file_expiry: int = Field(3600, env="TEMP_FILE_EXPIRY")
